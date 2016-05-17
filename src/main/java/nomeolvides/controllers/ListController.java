497a19f5f4760f447b362c92c8d3b92a805744f9
Eduardo Benito <bf_eduardo@yahoo.es>
2016-05-17 16:07:34 +0200

    ListController modified

--- src/main/java/nomeolvides/controllers/ListController.java
+++ src/main/java/nomeolvides/controllers/ListController.java
@@ -25,22 +25,21 @@ public class ListController {
 
 	private static Log log = LogFactory.getLog(ListController.class);
 
-	
 	// obtener todas las listas de un usuario
 	@RequestMapping(value = "/listas", method = RequestMethod.GET)
 	public List<Lista> getListasByUser(@RequestParam(value = "user") String user) {
-		log.info("GET /listas" + "-> user:" + user);
+		log.info("GET /listas" + " -> from user: " + user);
 		return listaRepository.findByUser(user);
 	}
 
-
+	// Obtener todas las listas de un usuario dado con el mismo nombre
 
 	// Dar de alta una lista nueva
 	@RequestMapping(value = "/listas", method = RequestMethod.POST)
 	public ResponseEntity<Boolean> addLista(@RequestBody Lista lista) {
 		try {
 			Lista l = listaRepository.save(lista);
-			log.info("POST /listas -> id lista:" + l.getId() + " added");
+			log.info("POST /listas -> id " + l.getId() + " added");
 			return new ResponseEntity<Boolean>(HttpStatus.OK);
 		} catch (Exception e) {
 
@@ -48,26 +47,44 @@ public class ListController {
 		return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
 	}
 
-	// Obtener todas las listas de un usuario dado con el mismo nombre
 
 	// Modificar una lista ya existente
-	@RequestMapping(value = "/lista/{name}", method = RequestMethod.PUT)
-	public ResponseEntity<Boolean> updateList(@PathVariable String name, @RequestBody Lista lista) {
-		log.info("PUT /lista/" + name + lista.getName());
+	@RequestMapping(value = "/lista/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Boolean> updateList(@PathVariable Long id, @RequestBody Lista lista) {
 		try {
-			Lista l = lista;
-			l.setName(name);
-			listaRepository.save(l);
+			if (listaRepository.exists(id)) {
+				Lista l = lista;
+				l.setId(id);
+				listaRepository.save(l);
+				log.info("PUT /lista/" + id + " -> id:" + l.getId() + " updated");
+				return new ResponseEntity<Boolean>(HttpStatus.OK);
+			}
 		} catch (Exception e) {
-
+			log.info("PUT /lista/" + id + " -> not updated");
+			return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
 		}
-		return null;
+		log.info("PUT /lista/" + id + " -> not updated");
+		return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
 	}
 
 	// borrar una lista dado su id
 	@RequestMapping(value = "/lista/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Boolean> deleteList(@PathVariable Long id) {
-		return null;
+		try {
+			listaRepository.findOne(id);
+			if (listaRepository.exists(id)) {
+				listaRepository.delete(listaRepository.findOne(id));
+				log.info("DELETE /lista/" + id + " -> id " + id + " deleted");
+				return new ResponseEntity<Boolean>(HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			log.info("DELETE /lista/" + id + " -> not deleted");
+			return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
+		}
+		log.info("DELETE /lista/" + id + " -> not deleted");
+		return new ResponseEntity<Boolean>(HttpStatus.NOT_FOUND);
+
 	}
 
 }
